37ad2ee98dff2fd17260a5443ac22057dc569f9d
duchainer <duchainer@protonmail.com>
2025-03-23 14:04:09 -0400

    Apply change from warning in README:
    > [!WARNING]
    > There's a bug in Odin right now that breaks the web build. As a temporary workaround, change `game.wasm.o` to `gamegame.wasm.o` in the build script.
    
    So I changed all the web build stuff to export and then use gamegame

--- build_github_pages.sh
+++ build_github_pages.sh
@@ -17,17 +17,17 @@ export EMSDK_QUIET=1
 # shellcheck disable=SC1091
 [[ -f "$EMSCRIPTEN_SDK_DIR/emsdk_env.sh" ]] && . "$EMSCRIPTEN_SDK_DIR/emsdk_env.sh"
 
-if ! odin-linux-amd64-nightly+2025-03-05/odin build source/main_web -target:freestanding_wasm32 -build-mode:obj -define:RAYLIB_WASM_LIB=env.o -vet -strict-style -o:speed -out:$OUT_DIR/game; then
+if ! odin-linux-amd64-nightly+2025-03-05/odin build source/main_web -target:freestanding_wasm32 -build-mode:obj -define:RAYLIB_WASM_LIB=env.o -vet -strict-style -o:speed -out:$OUT_DIR/gamegame; then
   exit 1
 fi
 
 ODIN_PATH=$(odin-linux-amd64-nightly+2025-03-05/odin root)
-files="$OUT_DIR/game.wasm.o ${ODIN_PATH}/vendor/raylib/wasm/libraylib.a ${ODIN_PATH}/vendor/raylib/wasm/libraygui.a"
+files="$OUT_DIR/gamegame.wasm.o ${ODIN_PATH}/vendor/raylib/wasm/libraylib.a ${ODIN_PATH}/vendor/raylib/wasm/libraygui.a"
 flags="-sUSE_GLFW=3 -sWASM_BIGINT -sWARN_ON_UNDEFINED_SYMBOLS=0 -sASSERTIONS --shell-file source/main_web/index_template.html --preload-file assets"
 
 # shellcheck disable=SC2086
 # Add `-g` to `emcc` call to enable debug symbols (works in chrome).
-emcc -g -o $OUT_DIR/index.html $files $flags && rm $OUT_DIR/game.wasm.o
+emcc -g -o $OUT_DIR/index.html $files $flags && rm $OUT_DIR/gamegame.wasm.o
 # emcc -o $OUT_DIR/index.html $files $flags
 
 echo "Web build created in ${OUT_DIR}"
