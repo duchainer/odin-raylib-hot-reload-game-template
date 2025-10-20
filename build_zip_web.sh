eb08adc34529d443e46125c58e0693daf4649f42
Raphaël Duchaîne <duchainer@protonmail.com>
2025-10-20 12:54:29 -0400

    Can build_zip_web.sh from the game.odin file
    Using:
     - Emacs compile-command definition with -*-
     - cd/pushd into the build_zip_web.sh folder
     - always popd at the end, even on script failure
        - We only do that when we reached it after the pushd

--- build_zip_web.sh
+++ build_zip_web.sh
@@ -1,1 +1,8 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd "$SCRIPT_DIR" > /dev/null || exit 1
+
+# Ensure popd runs on exit (success or failure)
+trap 'popd > /dev/null' EXIT
+
 rm -rf build/web/*; ./build_web.sh && zip -r build/web/web-game.zip build/web/*
